c664fb839b87d97a4a3539d182c52ac66eeb3b2c
Bernhard Haubold <haubold@evolbio.mpg.de>
2023-06-13 08:45:44 +0200

    Added switch for threads

--- findMacs/test.sh
+++ findMacs/test.sh
@@ -2,20 +2,36 @@ t=$(mktemp tXXX)
 n=$(mktemp nXXX)
 stan -s 3 -T $t -N $n -o
 r=$(mktemp rXXX.out)
-./findMacs $t $n > $r
+./findMacs $t $n 2>/dev/null > $r
 d=$(diff $r r1.txt)
-if [ "$d" = "" ]
+if [[ "$d" == "" ]]
 then
     echo "findMacs - mashmap OK"
 else
-    echo "Error: " $d
+    echo "Error - mashmap: " $d
+fi
+./findMacs -t 8 $t $n 2>/dev/null > $r
+d=$(diff $r r1.txt)
+if [[ "$d" == "" ]]
+then
+    echo "findMacs - mashmap -t OK"
+else
+    echo "Error - mashmap -t: " $d
 fi
 ./findMacs -n $t $n > $r
 d=$(diff $r r2.txt)
-if [ "$d" = "" ]
+if [[ "$d" == "" ]]
 then
     echo "findMacs - nucmer OK"
 else
-    echo "Error: " $d
+    echo "Error - nucmer " $d
+fi
+./findMacs -n -t 8 $t $n > $r
+d=$(diff $r r2.txt)
+if [[ "$d" == "" ]]
+then
+    echo "findMacs - nucmer -t OK"
+else
+    echo "Error - nucmer -t " $d
 fi
 rm -r $t $n $r
