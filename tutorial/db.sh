2b22c71e0734c133e6e03b752badc407f3e51242
Bernhard Haubold <haubold@evolbio.mpg.de>
2024-06-10 12:00:36 +0200

    Improve Makefile
    Tangling via noweb is only called if noweb exists.

--- tutorial/db.sh
+++
@@ -1,8 +0,0 @@
-wget guanine.evolbio.mpg.de/neighbors/neidb_210923.tgz
-tar -xvzf neidb_210923.tgz
-rm neidb_210923.tgz
-cd neidb_210923
-makeNeiDb
-cd ../
-mv neidb_210923/neidb .
-rm -r neidb_210923
