2c9bbd5c66fe6d4696338da0e3cb48131901f201
Bernhard Haubold <haubold@evolbio.mpg.de>
2026-04-08 09:47:37 +0200

    Added util.OutlierStatistics

--- util/util.go
+++ util/util.go
@@ -6,8 +6,20 @@ import (
 	"fmt"
 	"github.com/evolbioinf/clio"
 	"log"
+	"math"
 	"os"
+	"sort"
 )
+
+type OutlierStats struct {
+	LowerOuterFence float64
+	LowerInnerFence float64
+	LowerQuartile   float64
+	Median          float64
+	UpperQuartile   float64
+	UpperInnerFence float64
+	UpperOuterFence float64
+}
 
 var version, date string
 var name string
@@ -16,6 +28,7 @@ var assemblyLevels = []string{"complete",
 	"scaffold",
 	"contig"}
 
+// PrintInfo prints program information and exits.
 func PrintInfo(program string) {
 	author := "Bernhard Haubold"
 	email := "haubold@evolbio.mpg.de"
@@ -25,6 +38,8 @@ func PrintInfo(program string) {
 		author, email, license)
 	os.Exit(0)
 }
+
+// Open opens a file with error checking.
 func Open(file string) *os.File {
 	f, err := os.Open(file)
 	if err != nil {
@@ -34,20 +49,28 @@ func Open(file string) *os.File {
 	}
 	return f
 }
+
+// Check checks an error and aborts if it isn't nil.
 func Check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
+
+// The function SetName sets the name of the program.
 func SetName(n string) {
 	name = n
 	s := fmt.Sprintf("%s: ", n)
 	log.SetPrefix(s)
 	log.SetFlags(0)
 }
+
+// The function Version prints the version and other information about the program and exits.
 func Version() {
 	PrintInfo(name)
 }
+
+// The function LevelMsg prints the user message for the option directing the assembly level.
 func LevelMsg() string {
 	levels := assemblyLevels
 	m := "assembly-level: comma-delimited combination " +
@@ -58,4 +81,32 @@ func LevelMsg() string {
 	m += ", or " + levels[len(levels)-1]
 	m += " (default any)"
 	return m
+}
+
+// The function OutlierStatistics takes as argument a slice of floats and calculates outlier statistics as defined by the National Institute of Standards and Technology,  https://www.itl.nist.gov/div898/handbook/prc/section1/prc16.htm
+func OutlierStatistics(data []float64) *OutlierStats {
+	os := new(OutlierStats)
+	sort.Float64s(data)
+	n := len(data)
+	m := (n + 1) / 2
+	os.Median = data[m-1]
+	if n%2 == 0 {
+		os.Median = (os.Median + data[m]) / 2.0
+	}
+	exactQ := float64(n+1) * 0.25
+	f := math.Floor(exactQ)
+	l := int(f)
+	x := math.Remainder(exactQ, f)
+	os.LowerQuartile = data[l-1] + (data[l]-data[l-1])*x
+	exactQ = float64(n+1) * 0.75
+	f = math.Floor(exactQ)
+	l = int(f)
+	x = math.Remainder(exactQ, f)
+	os.UpperQuartile = data[l-1] + (data[l]-data[l-1])*x
+	r := os.UpperQuartile - os.LowerQuartile
+	os.LowerInnerFence = os.LowerQuartile - 1.5*r
+	os.UpperInnerFence = os.UpperQuartile + 1.5*r
+	os.LowerOuterFence = os.LowerQuartile - 3.0*r
+	os.UpperOuterFence = os.UpperQuartile + 3.0*r
+	return os
 }
--- util/util_test.go
+++ util/util_test.go
@@ -25,4 +25,17 @@ func TestUtil(t *testing.T) {
 			t.Errorf(m, level)
 		}
 	}
+	data := []float64{30, 171, 184, 201}
+	os := OutlierStatistics(data)
+	wr := []float64{-329.25, -132, 65.25, 177.5,
+		196.75, 394, 591.25}
+	gr := []float64{os.LowerOuterFence, os.LowerInnerFence,
+		os.LowerQuartile, os.Median, os.UpperQuartile,
+		os.UpperInnerFence, os.UpperOuterFence}
+	for i, w := range wr {
+		if w != gr[i] {
+			t.Errorf("want: %f\nget: %f\n",
+				w, gr[i])
+		}
+	}
 }
