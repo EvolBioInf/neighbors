fb627399c87b82ca5f669dd2302b4d053c04e6f4
Bernhard Haubold <haubold@evolbio.mpg.de>
2026-05-15 08:54:27 +0200

    Added fintac examples

---
+++ fintac/examples.sh
@@ -0,0 +1,9 @@
+# O111:H8 from paper
+./fintac -t 991910 ../data/phyl_cleaned.nwk
+./fintac -t 991910 -u 562 ../data/phyl_cleaned.nwk
+# O104:H4 from pangenomics chapter
+./fintac -t "1038927|1048254|1133852|1133853|1134782" ../data/phyl_cleaned.nwk
+./fintac -t "1038927|1048254|1133852|1133853|1134782" -u 562 ../data/phyl_cleaned.nwk
+# O157:H7
+./fintac -t 83334 ../data/phyl_cleaned.nwk
+./fintac -t 83334_ -u 562_ ../data/phyl_cleaned.nwk
