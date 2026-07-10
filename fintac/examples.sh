dda33870768ecb6b4a93b3c36d213b699e854a71
Bernhard Haubold <haubold@evolbio.mpg.de>
2026-07-10 17:47:21 +0200

    Updated fintac examples

--- fintac/examples.sh
+++ fintac/examples.sh
@@ -1,19 +1,21 @@
-p=./cmd/fintac
+prog=./cmd/fintac
+tree=../data/eco7k.nwk
+neidb=~/Data/neidb/neidb
 echo "***991910, O111:H8 from paper, -u"
-$p -t 991910 ../data/phyl_cleaned.nwk
-$p -t 991910 -u 562 ../data/phyl_cleaned.nwk
+$prog -t 991910_ $tree
+$prog -t 991910_ -u 562_ $tree
 echo "***O104:H4"
-$p -t "1038927|1048254|1133852|1133853|1134782" ../data/phyl_cleaned.nwk
-$p -t "1038927|1048254|1133852|1133853|1134782" -u 562 ../data/phyl_cleaned.nwk
+$prog -t "1038927|1048254|1133852|1133853|1134782" $tree
+$prog -t "1038927|1048254|1133852|1133853|1134782" -u 562 $tree
 echo "***O157:H7, -u, -H"
-$p -t 83334 ../data/phyl_cleaned.nwk
-$p -t 83334_ -u 562_ ../data/phyl_cleaned.nwk
-$p -t 83334_ -u 562_ -H ~/Data/neidb/neidb ../data/phyl_cleaned.nwk
+$prog -t 83334 $tree
+$prog -t 83334_ -u 562_ $tree
+$prog -t 83334_ -u 562_ -H $neidb $tree
 echo "***O25:H4-ST131, -u, -w"
-$p -t 941322_ ../data/phyl_cleaned.nwk
-$p -t 941322_ -u 562_ ../data/phyl_cleaned.nwk
+$prog -t 941322_ $tree
+$prog -t 941322_ -u 562_ $tree
 echo "***O16:H48 from pangenomics chapter and paper: include K12, -u, -H"
-$p -t 2605619_ ../data/phyl_cleaned.nwk
-$p -t '2605619_|83333_' ../data/phyl_cleaned.nwk
-$p -t '2605619_|83333_' -u 562_ ../data/phyl_cleaned.nwk
-$p -t '2605619_|83333_' -u 562_ -H ~/Data/neidb/neidb ../data/phyl_cleaned.nwk
+$prog -t 2605619_ $tree
+$prog -t '2605619_|83333_' $tree
+$prog -t '2605619_|83333_' -u 562_ $tree
+$prog -t '2605619_|83333_' -u 562_ -H $neidb $tree
