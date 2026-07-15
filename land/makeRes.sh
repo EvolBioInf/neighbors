1de701d284e8e9ec192c35f11091cad0e5751919
Bernhard Haubold <haubold@evolbio.mpg.de>
2026-07-15 17:13:57 +0200

    Changed logic of leaf label removal in land
    Previously removal of leaf labels (-l) coincided with relabeling of
    internal nodes. Now removal of leaf labels leaves internal nodes
    unchanged.

--- land/makeRes.sh
+++ land/makeRes.sh
@@ -1,5 +1,5 @@
-./land      test1.nwk > r1.txt
-./land -p p test1.nwk > r2.txt
-./land -s s test1.nwk > r3.txt
-./land -l   test1.nwk > r4.txt
-./land -r   test2.nwk > r5.txt
+./cmd/land      test1.nwk > r1.txt
+./cmd/land -p p test1.nwk > r2.txt
+./cmd/land -s s test1.nwk > r3.txt
+./cmd/land -l   test1.nwk > r4.txt
+./cmd/land -r   test2.nwk > r5.txt
