b3fac44def14d721935142e78de1425690ef33be
Ben Bahnsen <ben_bahnsen@web.de>
2026-07-21 11:12:09 +0200

    remote tests for dree

--- dree/dree_test.go
+++ dree/dree_test.go
@@ -8,45 +8,78 @@ import (
 	"testing"
 )
 
+type Test struct {
+	t *exec.Cmd
+	r int
+}
+
 func TestDree(t *testing.T) {
-	var tests []*exec.Cmd
+	var tests []Test
 	n := "207598"
 	d := "../data/test.db"
 	p := "./cmd/dree"
-	test := exec.Command(p, n, d)
+	test := Test{t: exec.Command(p, n, d), r: 1}
 	tests = append(tests, test)
-	test = exec.Command(p, "-n", n, d)
+	test = Test{t: exec.Command(p, "-D", "test", n, d), r: 1}
 	tests = append(tests, test)
-	test = exec.Command(p, "-g", n, d)
+	test = Test{t: exec.Command(p, "-n", n, d), r: 2}
 	tests = append(tests, test)
-	test = exec.Command(p, "-l", n, d)
+	test = Test{t: exec.Command(p, "-D", "test", "-n", n, d), r: 2}
 	tests = append(tests, test)
-	test = exec.Command(p, "-l", "-g", n, d)
+	test = Test{t: exec.Command(p, "-g", n, d), r: 3}
 	tests = append(tests, test)
-	test = exec.Command(p, "-l", "-n", n, d)
+	test = Test{t: exec.Command(p, "-D", "test", "-g", n, d), r: 3}
 	tests = append(tests, test)
-	test = exec.Command(p, "-l", "-g", "-n", n, d)
+	test = Test{t: exec.Command(p, "-l", n, d), r: 4}
 	tests = append(tests, test)
-	test = exec.Command(p, "-L", "complete", n, d)
+	test = Test{t: exec.Command(p, "-D", "test", "-l", n, d), r: 4}
 	tests = append(tests, test)
-	test = exec.Command(p, "-L", "complete,chromosome", n, d)
+	test = Test{t: exec.Command(p, "-l", "-g", n, d), r: 5}
 	tests = append(tests, test)
-	test = exec.Command(p, "-m", "-2", n, d)
+	test = Test{t: exec.Command(p, "-D", "test", "-l", "-g", n, d), r: 5}
 	tests = append(tests, test)
-	test = exec.Command(p, "-m", "0", n, d)
+	test = Test{t: exec.Command(p, "-l", "-n", n, d), r: 6}
 	tests = append(tests, test)
-	test = exec.Command(p, "-m", "2", n, d)
+	test = Test{t: exec.Command(p, "-D", "test", "-l", "-n", n, d), r: 6}
 	tests = append(tests, test)
-	test = exec.Command(p, "-m", "200", n, d)
+	test = Test{t: exec.Command(p, "-l", "-g", "-n", n, d), r: 7}
 	tests = append(tests, test)
-	test = exec.Command(p, "-R", "-l", n, d)
+	test = Test{t: exec.Command(p, "-D", "test", "-l", "-g", "-n", n, d), r: 7}
 	tests = append(tests, test)
-	for i, test := range tests {
-		get, err := test.Output()
+	test = Test{t: exec.Command(p, "-L", "complete", n, d), r: 8}
+	tests = append(tests, test)
+	test = Test{t: exec.Command(p, "-D", "test", "-L", "complete", n, d), r: 8}
+	tests = append(tests, test)
+	test = Test{t: exec.Command(p, "-L", "complete,chromosome", n, d), r: 9}
+	tests = append(tests, test)
+	test = Test{t: exec.Command(p, "-D", "test", "-L", "complete,chromosome", n, d), r: 9}
+	tests = append(tests, test)
+	test = Test{t: exec.Command(p, "-m", "-2", n, d), r: 10}
+	tests = append(tests, test)
+	test = Test{t: exec.Command(p, "-D", "test", "-m", "-2", n, d), r: 10}
+	tests = append(tests, test)
+	test = Test{t: exec.Command(p, "-m", "0", n, d), r: 11}
+	tests = append(tests, test)
+	test = Test{t: exec.Command(p, "-D", "test", "-m", "0", n, d), r: 11}
+	tests = append(tests, test)
+	test = Test{t: exec.Command(p, "-m", "2", n, d), r: 12}
+	tests = append(tests, test)
+	test = Test{t: exec.Command(p, "-D", "test", "-m", "2", n, d), r: 12}
+	tests = append(tests, test)
+	test = Test{t: exec.Command(p, "-m", "200", n, d), r: 13}
+	tests = append(tests, test)
+	test = Test{t: exec.Command(p, "-D", "test", "-m", "200", n, d), r: 13}
+	tests = append(tests, test)
+	test = Test{t: exec.Command(p, "-R", "-l", n, d), r: 14}
+	tests = append(tests, test)
+	test = Test{t: exec.Command(p, "-D", "test", "-R", "-l", n, d), r: 14}
+	tests = append(tests, test)
+	for _, test := range tests {
+		get, err := test.t.Output()
 		if err != nil {
-			t.Errorf("couldn't run %q", test)
+			t.Errorf("couldn't run %q", test.t)
 		}
-		f := "r" + strconv.Itoa(i+1) + ".txt"
+		f := "r" + strconv.Itoa(test.r) + ".txt"
 		want, err := ioutil.ReadFile(f)
 		if err != nil {
 			t.Errorf("couldn't open %q", f)
