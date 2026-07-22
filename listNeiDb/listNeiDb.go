8bfe9a3754d84741843ba820526f84a4cf6d0fbb
Ben Bahnsen <ben_bahnsen@web.de>
2026-07-22 15:47:23 +0200

    listNeiDb

---
+++ listNeiDb/listNeiDb.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/evolbioinf/clio"
+	"github.com/evolbioinf/neighbors/util"
+	"strings"
+)
+
+func main() {
+	util.SetName("listNeiDb")
+	u := "listNeiDb [option]"
+	p := "Print all of never's available databases."
+	e := "listNeiDb"
+	clio.Usage(u, p, e)
+	var optV = flag.Bool("v", false, "version")
+	flag.Parse()
+	if *optV {
+		util.PrintInfo("listNeiDb")
+	}
+	a := make(map[string]string)
+	a["plain_data"] = "true"
+	rawRes := util.SendGetRequest("api/v2/databases", nil, nil, a)
+	var res []string
+	json.Unmarshal([]byte(rawRes), &res)
+	fmt.Println(strings.Join(res, ", "))
+}
